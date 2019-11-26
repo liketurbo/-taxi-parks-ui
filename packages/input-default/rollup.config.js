ec99ea84f87855ea38bc05bdb854de1fc36ceb3b
Ramzan Chasygov <theliketurbo@gmail.com>
2019-11-26 15:14:33 +0300

    Updated dependencies

--- packages/input-default/rollup.config.js
+++ packages/input-default/rollup.config.js
@@ -1,5 +1,9 @@
 const rollupConfig = require("../../rollup.config")
 
 rollupConfig.output.name = "InputDefault"
+rollupConfig.external.push("@-taxi-parks-ui/input-base")
+rollupConfig.external.push("@-taxi-parks-ui/input-container")
+rollupConfig.external.push("@-taxi-parks-ui/input-error-msg")
+rollupConfig.external.push("@-taxi-parks-ui/input-star")
 
 module.exports = rollupConfig
--- packages/input-phone/rollup.config.js
+++ packages/input-phone/rollup.config.js
@@ -1,5 +1,10 @@
 const rollupConfig = require("../../rollup.config")
 
 rollupConfig.output.name = "InputPhone"
+rollupConfig.external.push("react-input-mask")
+rollupConfig.external.push("@-taxi-parks-ui/input-base")
+rollupConfig.external.push("@-taxi-parks-ui/input-container")
+rollupConfig.external.push("@-taxi-parks-ui/input-error-msg")
+rollupConfig.external.push("@-taxi-parks-ui/input-star")
 
 module.exports = rollupConfig
