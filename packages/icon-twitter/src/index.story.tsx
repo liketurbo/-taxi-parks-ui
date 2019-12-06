7a8021b5c96dfab2f26b47d8cac60ee355905098
Ramzan Chasygov <theliketurbo@gmail.com>
2019-12-06 15:58:14 +0300

    Added twitter icon

---
+++ packages/icon-twitter/src/index.story.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from "@storybook/react"
+import React from "react"
+
+import IconTwitter from "."
+
+storiesOf("Design System|Icons", module).add("twitter", () => <IconTwitter />)
---
+++ packages/icon-twitter/src/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+import PIconTwitter from "./social-icon_twitter.svg"
+
+const IconTwitter = styled(PIconTwitter)`
+  & path {
+    fill: white;
+
+    transition: fill 0.15s ease;
+  }
+
+  &:hover {
+    & path {
+      fill: #55acef;
+    }
+  }
+`
+
+export default IconTwitter
