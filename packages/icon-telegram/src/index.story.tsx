0cd8fe5d40f1f81c98e396763e6f5894981abc66
Ramzan Chasygov <theliketurbo@gmail.com>
2019-12-06 16:04:52 +0300

    Added telegram icon

---
+++ packages/icon-telegram/src/index.story.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from "@storybook/react"
+import React from "react"
+
+import IconTelegram from "."
+
+storiesOf("Design System|Icons", module).add("telegram", () => <IconTelegram />)
---
+++ packages/icon-telegram/src/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+import PIconTelegram from "./social-icon_telegram.svg"
+
+const IconTelegram = styled(PIconTelegram)`
+  & path {
+    fill: white;
+
+    transition: fill 0.15s ease;
+  }
+
+  &:hover {
+    & path {
+      fill: #31a9dd;
+    }
+  }
+`
+
+export default IconTelegram
