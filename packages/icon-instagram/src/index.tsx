49bbd14187548025878c5c77022967e1b415d289
Ramzan Chasygov <theliketurbo@gmail.com>
2019-12-08 02:35:10 +0300

    Fixed components

--- packages/icon-instagram/src/index.tsx
+++ packages/icon-instagram/src/index.tsx
@@ -1,41 +1,19 @@
-import React from "react"
-import styled, { CSSObject } from "styled-components"
+import styled from "styled-components"
 
 import PIconInstagram from "./social-icon_instagram.svg"
 
-declare const tw: (arg: TemplateStringsArray) => CSSObject
-
-const Container = styled.div`
-  ${tw`relative`}
-`
-
-const IconInstagramBleached = styled(PIconInstagram)`
-  ${tw`absolute opacity-100`}
-
-  transition: opacity 0.15s ease;
-
+const IconInstagram = styled(PIconInstagram)`
   & path {
     fill: #fff;
+
+    transition: fill 0.15s ease;
   }
 
   &:hover {
-    ${tw`opacity-0`}
+    & path {
+      fill: url(#a);
+    }
   }
 `
-
-const IconInstagramColored = styled(PIconInstagram)`
-  ${tw`absolute`}
-
-  & path {
-    fill: url(#a);
-  }
-`
-
-const IconInstagram = () => (
-  <Container>
-    <IconInstagramColored />
-    <IconInstagramBleached />
-  </Container>
-)
 
 export default IconInstagram
