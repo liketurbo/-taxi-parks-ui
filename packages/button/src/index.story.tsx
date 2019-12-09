51301401ed04789e32d884c9fa856419a79593a5
Ramzan Chasygov <theliketurbo@gmail.com>
2019-12-09 10:37:57 +0300

    Added "outiline" prop to Button

--- packages/button/src/index.story.tsx
+++ packages/button/src/index.story.tsx
@@ -1,99 +1,82 @@
-import { action } from "@storybook/addon-actions"
 import { storiesOf } from "@storybook/react"
 import React from "react"
 
-import Button from "."
+import Button, { ButtonProps } from "."
+
+const ButtonBundle = ({
+  variant,
+  outline,
+}: {
+  variant: ButtonProps["variant"]
+  outline?: ButtonProps["outline"]
+}) => (
+  <>
+    <div>
+      <Button
+        size="sm"
+        {...{
+          outline,
+          variant,
+        }}
+      >
+        {"Button"}
+      </Button>
+      <Button
+        disabled
+        size="sm"
+        {...{
+          outline,
+          variant,
+        }}
+      >
+        {"Button"}
+      </Button>
+    </div>
+    <div>
+      <Button
+        {...{
+          outline,
+          variant,
+        }}
+      >
+        {"Button"}
+      </Button>
+      <Button
+        disabled
+        {...{
+          outline,
+          variant,
+        }}
+      >
+        {"Button"}
+      </Button>
+    </div>
+    <div>
+      <Button
+        size="lg"
+        {...{
+          outline,
+          variant,
+        }}
+      >
+        {"Button"}
+      </Button>
+      <Button
+        disabled
+        size="lg"
+        {...{
+          outline,
+          variant,
+        }}
+      >
+        {"Button"}
+      </Button>
+    </div>
+    {!outline && <ButtonBundle outline {...{ variant }} />}
+  </>
+)
 
 storiesOf("Design System|Button", module)
-  .add("primary", () => (
-    <>
-      <div>
-        <Button onClick={action("onClick")} size="sm">
-          {"Button"}
-        </Button>
-        <Button disabled onClick={action("onClick")} size="sm">
-          {"Button"}
-        </Button>
-      </div>
-      <div>
-        <Button onClick={action("onClick")}>{"Button"}</Button>
-        <Button disabled onClick={action("onClick")}>
-          {"Button"}
-        </Button>
-      </div>
-      <div>
-        <Button onClick={action("onClick")} size="lg">
-          {"Button"}
-        </Button>
-        <Button disabled onClick={action("onClick")} size="lg">
-          {"Button"}
-        </Button>
-      </div>
-    </>
-  ))
-  .add("danger", () => (
-    <>
-      <div>
-        <Button onClick={action("onClick")} size="sm" variant="danger">
-          {"Button"}
-        </Button>
-        <Button disabled onClick={action("onClick")} size="sm" variant="danger">
-          {"Button"}
-        </Button>
-      </div>
-      <div>
-        <Button onClick={action("onClick")} variant="danger">
-          {"Button"}
-        </Button>
-        <Button disabled onClick={action("onClick")} variant="danger">
-          {"Button"}
-        </Button>
-      </div>
-      <div>
-        <Button onClick={action("onClick")} size="lg" variant="danger">
-          {"Button"}
-        </Button>
-        <Button disabled onClick={action("onClick")} size="lg" variant="danger">
-          {"Button"}
-        </Button>
-      </div>
-    </>
-  ))
-  .add("success", () => (
-    <>
-      <div>
-        <Button onClick={action("onClick")} size="sm" variant="success">
-          {"Button"}
-        </Button>
-        <Button
-          disabled
-          onClick={action("onClick")}
-          size="sm"
-          variant="success"
-        >
-          {"Button"}
-        </Button>
-      </div>
-      <div>
-        <Button onClick={action("onClick")} variant="success">
-          {"Button"}
-        </Button>
-        <Button disabled onClick={action("onClick")} variant="success">
-          {"Button"}
-        </Button>
-      </div>
-      <div>
-        <Button onClick={action("onClick")} size="lg" variant="success">
-          {"Button"}
-        </Button>
-        <Button
-          disabled
-          onClick={action("onClick")}
-          size="lg"
-          variant="success"
-        >
-          {"Button"}
-        </Button>
-      </div>
-    </>
-  ))
+  .add("primary", () => <ButtonBundle variant="primary" />)
+  .add("danger", () => <ButtonBundle variant="danger" />)
+  .add("success", () => <ButtonBundle variant="success" />)
--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -4,18 +4,27 @@ import styled, { css, CSSObject } from "styled-components"
 declare const tw: (arg: TemplateStringsArray) => CSSObject
 
 const SButton = styled.button<ButtonProps>`
-  ${tw`text-lg font-medium rounded py-2 px-4`}
+  ${tw`text-lg font-medium border rounded py-2 px-4`}
 
-  transition: background-color 0.15s ease;
+  transition: all 0.15s ease;
 
-  ${({ variant }) => {
+  ${({ outline, variant }) => {
     switch (variant) {
       case "danger":
-        return tw`bg-red hover:bg-red-dark text-grey-lightest`
+        if (outline)
+          return tw`bg-transparent hover:bg-red text-red hover:text-grey-lightest border-red`
+
+        return tw`bg-red hover:bg-red-dark text-grey-lightest border-red hover:border-red-dark`
       case "success":
-        return tw`bg-green hover:bg-green-dark text-grey-lightest`
+        if (outline)
+          return tw`bg-transparent hover:bg-green text-green hover:text-grey-lightest border-green border-green`
+
+        return tw`bg-green hover:bg-green-dark text-grey-lightest border-green hover:border-green-dark`
       default:
-        return tw`bg-yellow hover:bg-yellow-dark text-grey-darkest`
+        if (outline)
+          return tw`bg-transparent hover:bg-yellow text-yellow hover:text-grey-darkest border-yellow border-yellow`
+
+        return tw`bg-yellow hover:bg-yellow-dark text-grey-darkest border-yellow hover:border-yellow-dark`
     }
   }};
 
@@ -37,12 +46,12 @@ const SButton = styled.button<ButtonProps>`
 
       ${() => {
         switch (props.variant) {
-          case "success":
-            return tw`bg-green-dark`
           case "danger":
-            return tw`bg-red-dark`
+            return tw`bg-red-dark border-red-dark text-grey-lightest hover:bg-red-dark`
+          case "success":
+            return tw`bg-green-dark border-green-dark text-grey-lightest hover:bg-green-dark`
           default:
-            return tw`bg-yellow-dark`
+            return tw`bg-yellow-dark border-yellow-dark text-grey-darkest hover:bg-yellow-dark`
         }
       }}
     `}
@@ -57,6 +66,7 @@ const Button = ({
 export interface ButtonProps {
   variant?: "primary" | "danger" | "success"
   size?: "sm" | "md" | "lg"
+  outline?: boolean
 }
 
 export default Button
